651ea9fd88207350dd6ffc0290ba8e19ef176f1c
Magen Albritton <magenelise@gmail.com>
2022-03-25 12:16:13 -0500

    commit tree 5f1188306928ed39857e6301edd4ed61c6486116

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solutions in this file
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//     for(let i = 0; i < gifts.length; i++) {
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     }
+//     return gifts;
+// }
+// wrapGifts(gifts);
+
+// const cardRecipient = ["Luke", "Jonas", "Peter"];
+// const eventName =;
+
+const names = ["Luke", "Jonas", "Peter"];
+const eventName = "birthday";
+
+
+function writeCards(names, eventName) {
+    const message = [];
+    for (let i = 0; i < names.length; i++) {
+        message[i] = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`
+    }
+    return message;
+}
+
+let int = 10;
+function countDown(int) {
+    while (int>= 0) {
+        console.log(int--);
+    }
+    }; 
